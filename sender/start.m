17f2707f1a56f82506c3338d99bbdb2bc03d6c21
shikuzen <nakorn.k_s18@vistec.ac.th>
2024-05-16 20:28:03 +0200

    fix start 2

--- sender/start.m
+++ sender/start.m
@@ -1,10 +1,11 @@
-function start(ip, port)
+function start(ip1, ip2, port1, port2)
     fixport = 10000;
     u = udpport("IPV4", 'LocalPort', fixport);
     configureTerminator(u,"CR/LF");
     ip_client = ipread();
     msg = strcat("getframe","//",ip_client,"//", num2str(fixport));
-    writeline(u, msg, ip, port);    
+    writeline(u, msg, ip1, port1);
+    writeline(u, msg, ip2, port2);
     % resp = readline(u);
     % fprintf("msg resp: %s\n", resp);
     clear u;
